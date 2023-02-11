898e42b35ff161217b5443aea90addff4ebfcef1
Daria Amelchenya <remember.me397@gmail.com>
2023-02-11 18:45:15 +0300

    feat: added context

--- client/src/index.js
+++ client/src/index.js
@@ -1,16 +1,20 @@
 import React, { createContext } from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import UserProfile from './account/IsProfile';
+import Profile from './account/Profile';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import StudentProfile from './account/StudentProfile';
 
 export const Context = createContext(null)
+
+//console.log('==' + process.env.REACT_APP_API_URL)
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Context.Provider value={{
-      user: new UserProfile()
+      user: new Profile(),
+      students: new StudentProfile(),
     }}>
       <App />
     </Context.Provider>
