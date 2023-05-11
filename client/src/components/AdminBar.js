4428d5d09d834138f906c23572cf0a1cf69b3fd8
Daria Amelchenya <remember.me397@gmail.com>
2023-05-11 17:41:50 +0300

    fix: change the text in the button after click

--- client/src/components/AdminBar.js
+++ client/src/components/AdminBar.js
@@ -4,11 +4,11 @@ import { useLocation } from 'react-router-dom';
 const AdminBar = () => {
 	const location = useLocation()
 	return (
-		<Container style={{ width: 200 }} className='m-0'>
+		<Container style={{ width: 300 }} className='m-0'>
 			{<ListGroup defaultActiveKey={location.pathname}>
-				<ListGroup.Item action href="/queue">Очередь</ListGroup.Item>
-				<ListGroup.Item action href="/students">Заявки</ListGroup.Item>
-				<ListGroup.Item action href="/">Проживающие</ListGroup.Item>
+				<ListGroup.Item action href="/queue">Заявки</ListGroup.Item>
+				<ListGroup.Item action href="/students">Список студентов</ListGroup.Item>
+				<ListGroup.Item action href="/resident">Проживающие</ListGroup.Item>
 			</ListGroup>}
 		</Container >
 	);
