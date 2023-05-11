5963a8cd339bfbd379d8da16052b696088ce58f0
Daria Amelchenya <remember.me397@gmail.com>
2023-05-11 17:16:36 +0300

    fix: change text in button after click

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -5,12 +5,29 @@ import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import { LOGIN_ROUTE } from '../utils/consts';
 import { Button, Image } from 'react-bootstrap';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 // import { BsSnow } from 'react-icons/bs'
 import { observer } from "mobx-react-lite"
 
 const NavBar = observer(() => {
 	const { user } = useContext(Context)
+	const navigate = useNavigate()
+
+	if (!JSON.parse(localStorage.getItem('isAuth'))) {
+		user.setIsAuth(false)
+	} else {
+		user.setIsAuth(true)
+	}
+
+	const click = () => {
+		if (user._isAuth) {
+			localStorage.clear();
+		}
+		user.setIsAuth(!user._isAuth)
+		localStorage.setItem('isAuth', user._isAuth)
+		navigate(LOGIN_ROUTE)
+	}
+
 	return (
 		<Navbar bg="light" variant="light">
 			<Container>
@@ -18,10 +35,10 @@ const NavBar = observer(() => {
 					<Link className='nav-link' to={LOGIN_ROUTE}>Общежитие</Link>
 				</Navbar.Brand>
 				<Nav>
-					{user.isAuth ?
-						<Button className='ms-2' variant="outline-dark" onClick={() => user.setIsAuth(false)}>Выйти</Button>
+					{user._isAuth ?
+						<Button className='ms-2' variant="outline-dark" onClick={click}>Выйти</Button>
 						:
-						<Button className='ms-2' variant="outline-dark" onClick={() => user.setIsAuth(true)}>Войти</Button>
+						<Button className='ms-2' variant="outline-dark" onClick={click}>Войти</Button>
 					}
 				</Nav>
 			</Container>
